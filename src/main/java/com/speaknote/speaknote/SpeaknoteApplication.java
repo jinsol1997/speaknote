71a3c880d63809f93a0e61d2abb4796dc7793993
jinsol1997 <113006954+jinsol1997@users.noreply.github.com>
2025-12-04 01:15:54 +0900

    프로젝트 초기 생성

---
+++ src/main/java/com/speaknote/speaknote/SpeaknoteApplication.java
@@ -0,0 +1,13 @@
+package com.speaknote.speaknote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeaknoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeaknoteApplication.class, args);
+	}
+
+}
